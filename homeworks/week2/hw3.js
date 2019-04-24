64f98fe2a35f71497e1cc25891afbd144a3967c2
CindyLyu <xd753159@gmail.com>
2019-04-25 00:08:28 +0800

    Week2

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,9 @@
 function reverse(str) {
-  return str;
+  let c = '';
+  let i = str.length - 1;
+  for (i; i >= 0; i -= 1) {
+    c += str[i];
+  }
+  console.log(c);
 }
-
 reverse('hello');
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,20 @@
 function join(str, concatStr) {
-  return concatStr;
+  let result = '';
+  let i = 0;
+  for (i; i <= str.length - 2; i += 1) {
+    result = result + str[i] + concatStr;
+  }
+  return result + str[str.length - 1];
 }
 
 function repeat(str, times) {
-  return times;
+  let result = '';
+  let i = 1;
+  for (i; i <= times; i += 1) {
+    result += str;
+  }
+  return result;
 }
 
-console.log(join('a', '!'));
+console.log(join([1, 2, 3], ''));
 console.log(repeat('a', 5));
