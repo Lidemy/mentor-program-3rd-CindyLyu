0eacd803faf70fd2575d3bb0dca11158eb449715
CindyLyu <xd753159@gmail.com>
2019-05-14 13:04:23 +0800

    week2-challenge-opt

--- homeworks/week2/challenge.js
+++ homeworks/week2/challenge.js
@@ -1,25 +1,17 @@
 function search(arr, n) {
-  let i = arr.length;
-  do {
-    i = Math.floor(i / 2);
+  let L = 0;
+  let R = arr.length - 1;
+  while (L <= R) {
+    const i = Math.floor((L + R) / 2);
     if (arr[i] === n) {
       return i;
     }
-  } while (i > 2);
-  if (i <= 2 && arr[i] > n) {
-    for (i; i <= arr.length && i >= 0; i -= 1) {
-      if (arr[i] === n) {
-        return i;
-      }
+    if (arr[i] > n) {
+      R -= 1;
     }
-  }
-  if (i <= 2 && arr[i] < n) {
-    for (i; i <= arr.length; i += 1) {
-      if (arr[i] === n) {
-        return i;
-      }
+    if (arr[i] < n) {
+      L += 1;
     }
   }
   return -1;
 }
-console.log(search([1, 2, 10, 14, 39], 1));
