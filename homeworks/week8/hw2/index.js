e6cb9377963368d5c63fbff10408d62ac51c9a4a
CindyLyu <xd753159@gmail.com>
2019-06-21 16:21:55 +0800

    week8-hw2 留言及分頁修正

--- homeworks/week8/hw2/index.js
+++ homeworks/week8/hw2/index.js
@@ -3,13 +3,12 @@ const newpostSubmit = document.querySelector('.messageboard__newpost-submit');
 const newpostInput = document.querySelector('.messageboard__newpost-input');
 const changePage = document.querySelector('.messageboard__changepage');
 const latestPostRegion = document.querySelector('.messageboard__latestpost-region');
-let finalId;
 let page;
 let data;
 
 function countEnd(dataLength, clickPage) {
-  if ((dataLength - clickPage * 20 - 1) > 0) {
-    return dataLength - clickPage * 20 - 1;
+  if ((dataLength - clickPage * 20) > 0) {
+    return dataLength - clickPage * 20;
   }
   return 0;
 }
@@ -27,15 +26,15 @@ changePage.onclick = (e) => {
     let startID = dataLength - 1; // 當頁數為 1 時取的第一個 ID 值
     let endID = dataLength - 20; // 當頁數為 1 時取的最後一個 ID 值
     if (clickPage > 1) {
-      startID = dataLength - 21 * (clickPage - 1); // 當頁數大於 1 時取的第一個 ID 值
+      startID = dataLength - 21 * (clickPage - 1) + (clickPage - 1) - 1; // 當頁數大於 1 時取的第一個 ID 值
       endID = countEnd(dataLength, clickPage); // 當頁數大於 1 時取的最後一個 ID 值
     }
     latestPostRegion.innerHTML = '';
     for (let r = startID; r >= endID; r -= 1) {
       const latestPost = document.createElement('div');
       latestPost.classList.add('messageboard__latestpost');
-      const id = data[r].id;
-      const content = data[r].content;
+      const { id } = data[r];
+      const { content } = data[r];
       latestPost.innerHTML = `<div class='messageboard__latestpost-info'><i class="fas fa-user-circle"></i><span class='messageboard__latestpost-id'></span>${id}</div>
         <div class='messageboard__latestpost-content'>${content}</div>`;
       latestPostRegion.appendChild(latestPost);
@@ -45,41 +44,44 @@ changePage.onclick = (e) => {
 };
 
 // 當頁面載入時
-request.onload = () => {
-  if (request.status >= 200 && request.status < 400) {
-    data = JSON.parse(request.responseText);
-    page = Math.ceil((data.length) / 20);
-    finalId = data.length + 1;
-    for (let k = 1; k <= page; k += 1) {
-      const num = document.createElement('div');
-      num.classList.add('messageboard__changepage-child');
-      num.id = `page-button${k}`;
-      num.innerText = k;
-      if (k === 1) {
-        num.classList.add('active');
+function loadMessage() {
+  request.onload = () => {
+    if (request.status >= 200 && request.status < 400) {
+      data = JSON.parse(request.responseText);
+      page = Math.ceil((data.length) / 20);
+      changePage.innerHTML = '';
+      for (let k = 1; k <= page; k += 1) {
+        const num = document.createElement('div');
+        num.classList.add('messageboard__changepage-child');
+        num.id = `page-button${k}`;
+        num.innerText = k;
+        if (k === 1) {
+          num.classList.add('active');
+        }
+        changePage.appendChild(num);
       }
-      changePage.appendChild(num);
+      latestPostRegion.innerHTML = '';
+      for (let i = data.length - 1; i >= data.length - 20; i -= 1) {
+        const latestPost = document.createElement('div');
+        latestPost.classList.add('messageboard__latestpost');
+        const { id } = data[i];// 與 const id = data[i].id; 相同
+        const { content } = data[i];
+        latestPost.innerHTML = `<div class='messageboard__latestpost-info'><i class="fas fa-user-circle"></i><span class='messageboard__latestpost-id'></span>${id}</div>
+            <div class='messageboard__latestpost-content'>${content}</div>`;
+        latestPostRegion.appendChild(latestPost);
+      }
+    } else {
+      console.log('error', request.status);
     }
-    for (let i = data.length - 1; i >= data.length - 20; i -= 1) {
-      const latestPost = document.createElement('div');
-      latestPost.classList.add('messageboard__latestpost');
-      const id = data[i].id;
-      const content = data[i].content;
-      latestPost.innerHTML = `<div class='messageboard__latestpost-info'><i class="fas fa-user-circle"></i><span class='messageboard__latestpost-id'></span>${id}</div>
-          <div class='messageboard__latestpost-content'>${content}</div>`;
-      latestPostRegion.appendChild(latestPost);
-    }
-  } else {
-    console.log('error', request.status);
-  }
-};
+  };
+  request.onerror = () => {
+    console.log('error2', request.status);
+  };
+  request.open('GET', 'https://lidemy-book-store.herokuapp.com/posts?', true);
+  request.send();
+}
 
-request.onerror = () => {
-  console.log('error2', request.status);
-};
-
-request.open('GET', 'https://lidemy-book-store.herokuapp.com/posts?', true);
-request.send();
+loadMessage();
 
 // 提交新留言
 newpostSubmit.onclick = () => {
@@ -91,16 +93,7 @@ newpostSubmit.onclick = () => {
   request2.send(`content=${storePostInputValue}`);
   request2.onload = () => {
     if (request2.status >= 200 && request2.status < 400) {
-      alert('留言成功');
-      const latestPost = document.createElement('div');
-      latestPost.classList.add('messageboard__latestpost');
-      const id = finalId;
-      const content = storePostInputValue;
-      latestPost.innerHTML = `<div class='messageboard__latestpost-info'><i class="fas fa-user-circle"></i><span class='messageboard__latestpost-id'></span>${id}</div>
-          <div class='messageboard__latestpost-content'>${content}</div>`;
-      latestPostRegion.prepend(latestPost);
-    } else {
-      alert(`留言失敗，請稍後再試（錯誤碼：${request2.status}）`);
+      loadMessage();
     }
   };
 };
