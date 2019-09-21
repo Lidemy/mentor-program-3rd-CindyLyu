119252295213c664d0cbd582560915f3085327a0
CindyLyu <xd753159@gmail.com>
2019-09-21 15:22:18 +0800

    add week22 hw1~hw2 completed

---
+++ homeworks/week22/fe/hw1/src/BlogAbout.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+
+
+function BlogAbout() {
+  return (
+    <Fragment>
+      <section className="blog__about">
+        <img className='blog__about-img' src="https://fakeimg.pl/1200x1200/dbdbdb/?text=picture&font=bebas" />
+        <div className="blog__about-content">哩後，關於我關於我關於我甘魚我</div>
+      </section>
+    </Fragment>
+  );
+}
+
+
+export default BlogAbout;
